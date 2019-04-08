ef83c22de1e2fd1a26a9995abc0d086b2424cd17
Jairo Antonio Melo Flórez <jairomelo@users.noreply.github.com>
2019-04-08 10:34:51 -0500

    Añadido 'requests' como módulo requerido

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jairomelo/PARESS",
     packages=setuptools.find_packages(),
-    install_requires=['selenium','beautifulsoup4'],
+    install_requires=['selenium','beautifulsoup4','requests'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

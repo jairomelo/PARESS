249cb97b7ecdfdf14171e312d4dc8fd125327893
jairomelo <jairom@colmich.edu.mx>
2019-05-01 18:31:56 -0500

    mejoras a chromedriver

--- paress/configura.py
+++ paress/configura.py
@@ -6,7 +6,6 @@ import sys
 def imports():
 	try:
 		import reconex # Intento de solución de errores de conexión
-		import plataforma
 	except ImportError:
 		# Verifica que se encuentren disponibles los archivos adicionales
 		print(str(sys.exc_info()[1]),"No se encontró el archivo 'reconex.py'. Asegúrese de haberlo descargado y que esté en la carpeta principal del programa")

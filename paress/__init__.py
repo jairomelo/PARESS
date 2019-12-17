00ac36499cdc8bd31b2a5177dc9c29eac5df028f
Jairo Antonio Melo Flórez <jairomelo@users.noreply.github.com>
2019-12-17 12:25:26 -0600

    Update __init__.py

--- paress/__init__.py
+++ paress/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from .compatibilidad import get_chromedriver_url
 from .compatibilidad import install_chromedriver
-# from .paress import imagenes,metadata,metadatalist,version
+from .paress import imagenes,metadata,metadatalist,version
 from .configura import imports
 from .plataforma import check_install
 from .plataforma import navegador
